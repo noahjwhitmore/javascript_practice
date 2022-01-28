ceec18d4f1f806accd24dea4f3bf6a573ecdf236
Noah Whitmore <noahjwhitmore@gmail.com>
2022-01-28 12:50:10 -0600

    Totally changed script.js

--- script.js
+++ script.js
@@ -1,27 +1,23 @@
-var names = ["Hole in one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+function caseInSwitch(val) {
+  var answer = "";
 
-function golfScore(par, strokes) {
-
-  if (strokes === 1) {
-    return names[0];
+  switch (val) {
+    case 1:
+      answer = "alpha";
+      break;
+    case 2:
+      answer = "beta";
+      break;
+    case 3:
+      answer = "cappa";
+      break;
+    case 4:
+      answer = "delta";
+      break;
   }
 
-  var score = strokes - par;
-
-  if (score <= -2) {
-    return names[1];
-  } else if (score === -1) {
-    return names[2];
-  } else if (score === 0) {
-    return names[3];
-  } else if (score === 1) {
-    return names[4];
-  } else if (score === 2) {
-    return names[5];
-  } else {
-    return names[6];
-  }
+  return answer;
 
 }
 
-console.log(golfScore(6, 5));
+console.log(caseInSwitch(4));
