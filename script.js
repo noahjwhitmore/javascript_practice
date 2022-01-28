76aff4fdca55436f84b88683b43d4693e4e47871
Noah Whitmore <noahjwhitmore@gmail.com>
2022-01-28 12:13:29 -0600

    Another CHange

--- script.js
+++ script.js
@@ -1,20 +1,5 @@
-var myGlobal = 50;
-
-function fun1() {
-  var oopsGlobal = 5;
+function minusSeven(num) {
+  return num - 7;
 }
 
-function fun2() {
-  var output = "";
-  if (typeof myGlobal != "undefined") {
-    output += "myGlobal is a global variable, and it is equal to " + myGlobal;
-  }
-  if (typeof oopsGlobal != "undefined") {
-    output += "\noopsGlobal is also global, and it is equal to " + oopsGlobal;
-  }
-
-  console.log(output);
-}
-
-fun1();
-fun2();
+console.log(minusSeven(10))
