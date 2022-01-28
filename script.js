82973f6bc5102a42ccaf5a31f4a6483c140d7a1c
Noah Whitmore <noahjwhitmore@gmail.com>
2022-01-28 10:39:27 -0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+let firstName = 'Noah';
+let thirdLetter = firstName[2];
+
+console.log('Third letter is', thirdLetter);
+
+let lastLetter = firstName[firstName.length - 1];
+
+console.log('last letter is', lastLetter);
