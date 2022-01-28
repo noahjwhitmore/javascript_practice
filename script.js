66df9ea0f1e4ad8c895ff39992688654f41447b0
Noah Whitmore <noahjwhitmore@gmail.com>
2022-01-28 11:17:39 -0600

    Final commit before changing

--- script.js
+++ script.js
@@ -10,3 +10,15 @@ var removedFromArray = theArray.pop();
 
 console.log("removed", removedFromArray);
 console.log("the array that's left", theArray);
+
+
+// Shift to remove the first array item, removed item is returned
+
+var removedFromArray = theArray.shift();
+console.log("removed", removedFromArray);
+console.log("the array that's left", theArray);
+
+// Unshift to add item to beginning of array
+
+theArray.unshift("Homer");
+console.log(theArray);
